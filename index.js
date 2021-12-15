90b3a105cb54cb629907854e6dd4d560fbdaa4f2
arafathsiam99 <md.arafathislamsiamnstuam14@gmail.com>
2021-12-15 11:10:36 +0600

    updated

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
+
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const uri =
+  "mongodb+srv://gramoday:newuser123456@cluster0.hahq7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("product");
+    const productsCollection = database.collection("products");
+
+    // POST API
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await productsCollection.insertOne(newUser);
+      console.log(newUser);
+      // console.log("got new user", req.body);
+      // console.log("got new user", result);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello From Gramoday");
